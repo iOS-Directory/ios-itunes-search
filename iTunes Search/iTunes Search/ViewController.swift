6639485427d00ac1c896b1108fa311210d2302bc
fritzgt <tuanis@live.com>
2020-10-23 10:21:12 +0300

    implementing table view delegate

--- iTunes Search/iTunes Search/ViewController.swift
+++ iTunes Search/iTunes Search/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
 
+    //MARK: - Properties
+    
+    
+    //MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+//MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        
+        return cell
+    }
+    
+    
+}
+
+//MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate{
+    
+    
+}
