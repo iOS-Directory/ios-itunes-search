e4e5548ba42dd697398722ab30784249c9da9dfe
fritzgt <tuanis@live.com>
2020-10-26 11:28:24 +0200

    create result type model

---
+++ iTunes Search/iTunes Search/Model/ResultType.swift
@@ -0,0 +1,16 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by FGT MAC on 10/26/20.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
+
+
